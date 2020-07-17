596dd5d2b97db263f02b4a6f29c1cfbcf6039a48
Wilson Ng <wilsongch92@gmail.com>
2020-07-17 01:33:26 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        :name => "Rome",
+        :name => "Romeo",
         :age => "15",
         :status => "alive"
         },
